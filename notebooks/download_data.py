855da6d53715edb22b1f00e327cc9f74099c3162
michael960725 <0725michael@gmail.com>
2021-07-14 11:05:31 +0900

    [#3] update doanload_daata.py

--- notebooks/download_data.py
+++ notebooks/download_data.py
@@ -4,12 +4,13 @@ import pandas as pd
 # import urllib.request
 
 # Data path
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/Jaehun-Kim22/SCIC_Project/blob/feature/2/"
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/Jaehun-Kim22/SCIC_Project/master/"
 DATA_PATH = os.path.join("assets")
-DATA_URL = DOWNLOAD_ROOT + "assets/학습데이터.csv"
+DATA_URL = DOWNLOAD_ROOT + "assets/training_data.csv"
+
 
 print(DATA_URL)
-def load_data(data_path=DATA_URL):
+def load_data(data_path=DATA_URL, encoding = 'utf-8'):
     return pd.read_csv(data_path)
 
 
